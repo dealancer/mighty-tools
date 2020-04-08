83061488d201a2c1f9c19fba420e705489bfc0b8
Vadym Myrgorod <dealancer@gmail.com>
2020-04-08 00:03:26 -0400

    Adding iCloud Photo Converter

---
+++ README.md
@@ -0,0 +1,29 @@
+# mighty-tools
+
+## iCloud photo converter
+
+### Usage
+```
+./icloud-photo-conv.sh input-path output-path
+```
+
+### Dependencies
+* `exiftool`
+* `magick`
+* `ffmpeg`
+
+Use `homebrew` to install these depdencies.
+
+### Features
+* Converts HEIC files to JPG ones.
+* Converts MOV files to MP4 ones.
+* Preserves metadata.
+* Uses creation date as a filename.
+* Categorizes files into directories by year and month.
+
+
+
+
+
+
+
---
+++ icloud-photo-conv.sh
@@ -0,0 +1,43 @@
+#/bin/sh
+set -e
+
+if [ $# -eq 0 ]
+then
+  echo "usage: ./icloud-photo-conv.sh input-path output-path"
+  exit
+fi
+
+INPUT_PATH=$1
+OUTPUT_PATH=$2
+
+[ ! -d "$OUTPUT_PATH" ] && echo "Output path does not exists." && exit
+
+for FILE_NAME in `ls "$INPUT_PATH"`
+do
+  #CREATED_DATE=$(mdls -name kMDItemContentCreationDate "$INPUT_PATH/$FILE_NAME" -raw)
+  CREATED_DATE=$(exiftool -s -s -s -createdate "$INPUT_PATH/$FILE_NAME")
+
+  if [ -z "$CREATED_DATE" ]
+  then
+    NEW_SUB_DIR_NAME="_nodate"
+    NEW_FILE_NAME="$(echo $FILE_NAME | md5 | cut -c1-8)"
+  else
+    NEW_SUB_DIR_NAME=$(date -j -f %Y:%m:%d\ %H:%M:%S "$CREATED_DATE" +%Y-%m)
+    NEW_FILE_NAME="$(date -j -f %Y:%m:%d\ %H:%M:%S "$CREATED_DATE" +%Y%m%d-%H%m%S)-$(echo $FILE_NAME | md5 | cut -c1-8)"
+  fi
+
+  [ -d "$OUTPUT_PATH/$NEW_SUB_DIR_NAME" ] || mkdir "$OUTPUT_PATH/$NEW_SUB_DIR_NAME"
+  EXTENTION=$(echo $FILE_NAME | awk -F . '{print $NF}' | tr '[:lower:]' '[:upper:]')
+
+  echo "Processing $FILE_NAME..."
+
+  if [ $EXTENTION = "HEIC" ]
+  then
+    magick convert "$INPUT_PATH/$FILE_NAME" "$OUTPUT_PATH/$NEW_SUB_DIR_NAME/$NEW_FILE_NAME.JPG"
+  elif [ $EXTENTION = "MOV" ]
+  then
+    ffmpeg -y -hide_banner -loglevel panic -threads 4 -i "$INPUT_PATH/$FILE_NAME" -map_metadata 0 -c copy -c:a aac "$OUTPUT_PATH/$NEW_SUB_DIR_NAME/$NEW_FILE_NAME.MP4"
+  else
+    cp "$INPUT_PATH/$FILE_NAME" "$OUTPUT_PATH/$NEW_SUB_DIR_NAME/$NEW_FILE_NAME.$EXTENTION"
+  fi
+done
